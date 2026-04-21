69ee66dc3f2bf72fb3146feb9521fd585a2236f3
Jose Allona <jallona@gmail.com>
2026-04-20 21:23:45 -0300

    auto: frontend/next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  webpack: (config) => {
-    const path = require("path");
-    const d3Path = path.resolve(__dirname, "node_modules/d3/dist/d3.js");
-    config.resolve.alias["d3"] = d3Path;
-    config.module.rules.push({
-      test: d3Path,
-      type: "javascript/auto",
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
