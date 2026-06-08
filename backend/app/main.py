81f7e20070721869920f02449394951d29c8e653
Jose Allona <jallona@gmail.com>
2026-06-08 19:41:26 -0300

    auto: backend/app/main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,6 +24,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://frontend:3000",
+        "http://localhost:3002",
+        "http://127.0.0.1:3002",
     ],
     allow_credentials=True,
     allow_methods=["*"],
